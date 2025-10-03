b2b6630b0a5aa02287d22d869845dd8109150d12
amirfareed787898 <amirfareed787898@gmail.com>
2025-10-03 11:50:29 +0000

    Fix IllegalStateException and update to SDK 35

--- app/src/main/java/com/coheser/app/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/coheser/app/adapters/ViewPagerAdapter.java
@@ -5,21 +5,22 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ViewPagerAdapter extends FragmentStateAdapter {
-    private  List<Fragment> mFragmentList = new ArrayList<>();
-    FragmentManager fragmentManager;
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
     public ViewPagerAdapter(Fragment fragment) {
         super(fragment);
     }
+
     public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
         super(fragmentManager, lifecycle);
-        mFragmentList.clear();
-        this.fragmentManager=fragmentManager;
     }
 
 
@@ -30,44 +31,27 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        if(mFragmentList.get(position).isAdded() && fragmentManager!=null){
-            fragmentManager.beginTransaction().remove(mFragmentList.get(position)).commit();
-        }
         return mFragmentList.get(position);
     }
 
 
-    public void addFrag(Fragment fragment) {
+    public void addFrag(Fragment fragment, String title) {
         mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
     }
 
-    // Set fragments dynamically
-    public void setFragments( List<Fragment> fragments) {
-        mFragmentList.clear();
-        mFragmentList.addAll(fragments);
-        notifyDataSetChanged();
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
     }
 
-    public Fragment getFragments(int pos) {
-        return mFragmentList.get(pos);
+    public List<Fragment> getFragments() {
+        return mFragmentList;
     }
-
-    public void removeFrag(int index) {
-
-        mFragmentList.remove(index);
-    }
-
 
     public void clearFragments() {
-        if(fragmentManager!=null) {
-            for (int i = 0; i < mFragmentList.size(); i++) {
-                if(mFragmentList.get(i).isAdded() && fragmentManager!=null){
-                    fragmentManager.beginTransaction().remove(mFragmentList.get(i)).commit();
-                }
-            }
-        }
-
         mFragmentList.clear();
+        mFragmentTitleList.clear();
+        notifyDataSetChanged();
     }
 
     @Override
@@ -75,6 +59,20 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
         return mFragmentList.size();
     }
 
+    @Override
+    public long getItemId(int position) {
+        // Give an ID to each fragment
+        return mFragmentList.get(position).hashCode();
+    }
 
-
+    @Override
+    public boolean containsItem(long itemId) {
+        // Check if the item is still in the dataset
+        for (Fragment fragment : mFragmentList) {
+            if (fragment.hashCode() == itemId) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
